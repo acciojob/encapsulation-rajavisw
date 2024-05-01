d1a91a32bcd0da0e3c81af954afddcf0ae9939eb
rajavisw <rajaviswanadhapalli12@gmail.com>
2024-05-01 17:30:05 +0530

    Assignment submitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args){
         RWOnly obj=new RWOnly();
-        obj.name="sai";  //name has private access in com.driver.RWOnly
-        System.out.println(obj.name);
+//        obj.name="sai";  //name has private access in com.driver.RWOnly
+//        System.out.println(obj.name);
         obj.setName("Sai");
         String name=obj.getName();
     }
